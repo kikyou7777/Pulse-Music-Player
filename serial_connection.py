dfc9161ba40ca55698a3f5a85f8ad50cb1680623
Shirley Li <shirleyli7777@gmail.com>
2024-04-21 14:12:29 -0400

    added in_waiting

--- serial_connection.py
+++ serial_connection.py
@@ -3,18 +3,25 @@ import serial
 def setup_serial(port='COM4', baudrate=115200, timeout=0.1):
     return serial.Serial(port, baudrate, timeout)
 
-def read():
-    # Update the port later
-    arduino = serial.Serial(port = 'COM4', baudrate = "115200", timeout =.1)
-
-    #https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
-    #Used this to create serial connection, also asked ChatGPT, and it recommended adding "in_waiting"
-    while True:
-        if arduino.in_waiting > 0:
-            data = arduino.readline()
-            minBPM = data.split()[0]
-            maxBPM = data.split()[1]
-
-            return minBPM, maxBPM
-
+# def read():
+#     # Update the port later
+#     arduino = serial.Serial(port = 'COM4', baudrate = "115200", timeout =.1)
+#
+#     #https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
+#     #Used this to create serial connection, also asked ChatGPT, and it recommended adding "in_waiting"
+#     while True:
+#         if arduino.in_waiting > 0:
+#             data = arduino.readline()
+#             minBPM = data.split()[0]
+#             maxBPM = data.split()[1]
+#
+#             return minBPM, maxBPM
     
+def read_bpm(arduino):
+    if arduino.in_waiting > 0:
+        line = arduino.readline().decode().strip()
+        try:
+            return map(int, line.split())
+        except ValueError:
+            print("Failed to parse BPM values")
+    return None, None
