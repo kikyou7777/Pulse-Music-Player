39ea86428792355901bc7453ece9cd58559ca019
Shirley Li <shirleyli7777@gmail.com>
2024-04-21 14:09:39 -0400

    simplify the authorization.py and used it in musicplayer.py

--- spotify_authorization.py
+++ spotify_authorization.py
@@ -2,16 +2,13 @@ import spotipy
 
 from spotipy.oauth2 import SpotifyOAuth
 
-client_id = "543e98d50d59492fa7cf308ef34b0257"
-client_secret = "3ef131df79ae4d019798200cd79c1ea4"
-redirect_uri = 'http://localhost:8888/callback'
-scope = "user-read-private user-modify-playback-state"
-
-
-def authorization():
-    #https://spotipy.readthedocs.io/en/2.6.1/
-    auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri= redirect_uri, scope = scope)
-    sp = spotipy.Spotify(auth_manager=auth_manager)
-
-
-    return sp
+def get_spotify_client():
+    client_id = "543e98d50d59492fa7cf308ef34b0257"
+    client_secret = "3ef131df79ae4d019798200cd79c1ea4"
+    redirect_uri = 'http://localhost:8888/callback'
+    scope = "user-read-private user-modify-playback-state"
+    auth_manager = SpotifyOAuth(client_id=client_id,
+                                client_secret=client_secret,
+                                redirect_uri=redirect_uri,
+                                scope=scope)
+    return spotipy.Spotify(auth_manager=auth_manager)
--- spotify_music_player.py
+++ spotify_music_player.py
@@ -1,33 +1,15 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
+#querySongs.py has been replaced with spotify_music_player.py which now handles both querying and playing songs.
 class SpotifyMusicPlayer:
-    def __init__(self, client_id, client_secret, redirect_uri):
-        self.scope = 'user-modify-playback-state user-read-playback-state user-read-private'
-        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-            client_id=client_id,
-            client_secret=client_secret,
-            redirect_uri=redirect_uri,
-            scope=self.scope
-        ))
-        self.last_bpm = None  # Store the last BPM that triggered a song change
-        self.threshold = 10   # BPM change threshold to trigger a new song
+    def __init__(self):
+        self.sp = get_spotify_client()
 
-
-    def play_song(self, bpm):
-        if self.last_bpm is None or abs(self.last_bpm - bpm) >= self.threshold:
-            # Only play a new song if the BPM change exceeds the threshold or if no song has been played yet
-            self.last_bpm = bpm  # Update the last BPM
-            tracks = self.sp.recommendations(seed_genres=['workout'], limit=1, target_tempo=bpm)['tracks']
-            if tracks:
-                track_uri = tracks[0]['uri']
-                self.sp.start_playback(uris=[track_uri])
-                print(f"Playing: {tracks[0]['name']} by {tracks[0]['artists'][0]['name']} at {bpm} BPM")
-
-    def get_current_device(self):
-        """ Returns the first available device to control playback. """
-        devices = self.sp.devices()
-        for device in devices['devices']:
-            if device['is_active']:
-                return device['id']
-        return None
+    def play_songs(self, min_BPM, max_BPM):
+        recommendations = self.sp.recommendations(seed_genres=['workout'], limit=1,
+                                                  min_tempo=min_BPM, max_tempo=max_BPM,
+                                                  target_danceability=0.6, min_energy=0.6)
+        if recommendations['tracks']:
+            track_uri = recommendations['tracks'][0]['uri']
+            self.sp.start_playback(uris=[track_uri])
+            print(f"Playing: {recommendations['tracks'][0]['name']}")
